387ee4e209c2d14f5579c7a2f71daf12e9680fed
Pierre Lecerf <pierre.lecerf@gmail.com>
2022-11-21 01:24:15 +0100

    Text: upgrading word-count sample

--- sample/wordcount/src/java/com/noleme/flow/connect/wordcount/Main.java
+++ sample/wordcount/src/java/com/noleme/flow/connect/wordcount/Main.java
@@ -3,11 +3,11 @@ package com.noleme.flow.connect.wordcount;
 import com.noleme.flow.Flow;
 import com.noleme.flow.compiler.CompilationException;
 import com.noleme.flow.compiler.RunException;
+import com.noleme.flow.connect.commons.FlowIO;
 import com.noleme.flow.connect.commons.accumulator.Accumulators;
 import com.noleme.flow.connect.commons.generator.ReaderGenerator;
 import com.noleme.flow.connect.commons.loader.file.FileWriteString;
 import com.noleme.flow.connect.commons.transformer.Transformers;
-import com.noleme.flow.connect.commons.transformer.filesystem.FileStreamer;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,7 +21,7 @@ public class Main
     public static void main(String[] args) throws CompilationException, RunException
     {
         var flow = Flow
-            .from(new FileStreamer(), "data/test.txt")
+            .from(FlowIO::streamFile, "data/test.txt")
             .stream(ReaderGenerator::new)
             .pipe(line -> List.of(line.split("[^\\p{L}]+")))
             .accumulate(Accumulators::concat)
